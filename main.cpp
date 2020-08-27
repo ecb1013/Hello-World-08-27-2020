f9aa93477d11f154b0ac851562401c0b217eb77e
ecb1013 <67292050+ecb1013@users.noreply.github.com>
2020-08-27 12:14:19 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Emma Banuelos
+ * Created on August 27, 2020, 12:03 PM
+ * Purpose:  Hello World Intro Lab
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - No Global Variables
+//Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    
+    
+    
+    //Initialize Variables
+    
+    
+    //Paint the fence front and back twice
+    
+    
+    //Display Outputs
+    cout<<"Hello World"<<endl;
+
+    //Exit stage right!
+    return 0;
+}
